6e5858a01805ff10e28247e1842fab1c668b38a9
Jhuni0123 <whdgnsdl887@naver.com>
2016-11-29 02:24:23 +0900

    make Pair hashable

--- src/jnDB/Pair.java
+++ src/jnDB/Pair.java
@@ -8,4 +8,21 @@ public class Pair<F,S> {
 		first = fst;
 		second = snd;
 	}
+
+	@Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof Pair)){ return false; }
+        Pair<F,S> pair = (Pair<F,S>)obj;
+        System.out.println("qqqqqq");
+		return (this.first == pair.first || (this.first!=null && this.first.equals(pair.first)))
+					&& (this.second == pair.second || (this.second!= null && this.second.equals(pair.second)));
+    }
+	
+	@Override
+	public int hashCode(){
+		if(first == null&&second==null)return 0;
+		if(first == null)return second.hashCode();
+		if(second == null)return first.hashCode();
+		return first.hashCode() ^ second.hashCode();
+	}
 }
